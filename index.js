c17e3a5c8a7dc8f1f0b5a71a0d67b2eaa5be7b08
henri-kapidani <h.kapidani@gmail.com>
2023-12-06 11:46:44 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+const express = require('express')
+const morgan = require('morgan')
+var cors = require('cors')
+
+const PORT = 3000
+const app = express()
+
+app.use(cors())
+app.use(morgan('tiny'))
+
+app.get('/status/:statusCode/delay/:delayInSeconds', (req, res) => {
+	setTimeout(() => {
+		const statusCode = parseInt(req.params.statusCode)
+		res.status(statusCode)
+		res.send({ statusCode })
+	}, req.params.delayInSeconds * 1000)
+})
+
+app.listen(3000, () => console.log(`Server is running at port ${PORT}`))
